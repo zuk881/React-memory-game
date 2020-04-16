2ef462e9cbfef91db56f1b106d769eee97ac14f6
zuk881 <zuk881@cox.net>
2020-04-15 19:22:39 -0700

    Updates

---
+++ precache-manifest.6f3e4adb85e27db0ca1d7962ff546e61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb66d0ebc7bd4c7ca16e13c330321be0",
+    "url": "/React-memory-game/index.html"
+  },
+  {
+    "revision": "820689de517e386ec25e",
+    "url": "/React-memory-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b88be3078e018e6e2319",
+    "url": "/React-memory-game/static/js/2.e11f3b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-memory-game/static/js/2.e11f3b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820689de517e386ec25e",
+    "url": "/React-memory-game/static/js/main.03661b86.chunk.js"
+  },
+  {
+    "revision": "ab452ea30c40f68eff23",
+    "url": "/React-memory-game/static/js/runtime-main.0a86ae18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
